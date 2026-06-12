c5daa1d01b7658e4550a930988a7293a399a9456
natrajanmd-579 <natrajanmd579@gmail.com>
2026-06-12 11:51:47 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
@@ -6,19 +6,21 @@ import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import "./App.css";
+
 import AOS from "aos";
 import "aos/dist/aos.css";
-import { useEffect } from "react";
-useEffect(() => {
-  AOS.init({ duration: 1000 });
-}, []);
 
 function App() {
+
   const [theme, setTheme] = useState("dark");
 
   const toggleTheme = () => {
     setTheme(theme === "dark" ? "light" : "dark");
   };
+
+  useEffect(() => {
+    AOS.init({ duration: 1000 });
+  }, []);
 
   return (
     <div className={theme}>
